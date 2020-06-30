9034e315ba0145f8924483c6779936ba7e285029
norzest <51143484+norzest@users.noreply.github.com>
2020-06-30 21:22:11 +0900

    Add files via upload

--- Raspberry_Deeplearning/video_thread_test.py
+++ Raspberry_Deeplearning/video_thread_test.py
@@ -4,7 +4,6 @@ import cv2
 import numpy as np
 import time
 import threading
-import keyboard
 import firebase_admin
 import random
 from tensorflow.python.compiler.tensorrt import trt_convert as trt
@@ -13,8 +12,32 @@ from tensorflow.python.saved_model import tag_constants
 from tensorflow.python.framework import convert_to_constants
 import picamera
 from time import sleep
+from uuid import uuid4
 from firebase_admin import credentials
 from firebase_admin import db
+from firebase_admin import storage
+
+cred = credentials.Certificate('myKey.json')
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://graduate-work-462b3.firebaseio.com',
+    'storageBucket': "graduate-work-462b3.appspot.com"
+})
+
+
+bucket = storage.bucket()
+
+def fileUpload(file):
+    blob = bucket.blob(file)
+    #new token and metadata 설정
+    print("save start")
+    new_token = uuid4()
+    metadata = {"firebaseStorageDownloadTokens": new_token} #access token이 필요하다.
+    blob.metadata = metadata
+
+    #upload file
+    blob.upload_from_filename(filename=file, content_type='image/png')
+    print(blob.public_url)
+
 
 
 def recordVideo():
@@ -28,15 +51,21 @@ def recordVideo():
         camera.stop_recording()
         camera.stop_preview()
 
-def record(name1, name2):
+def record(cond,turn):
     while True:
-        print(name1)
+        cond.acquire()  ### mutex_lock
+        while turn.myTurn != 0: cond.wait()
+        print('record run')
         recordVideo()
-        print(name2)
+        turn.myTurn = 1
+
+        cond.notifyAll()  # notify to all consumers
+        cond.release() ### mutex_unlock
+        print('record end')
 
 
-def deeplearning(name1, name2):
-    print(name1)
+def deeplearning(cond,turn):
+    print('deeplearning start')
     inputs = tf.placeholder(tf.float32, [None, 416, 416, 3])
     model = nets.YOLOv3COCO(inputs, nets.Darknet19)
     # model = nets.YOLOv2(inputs, nets.Darknet19)
@@ -48,69 +77,85 @@ def deeplearning(name1, name2):
 
     # list_of_classes=[0,1,2,3,5,7]
     list_of_classes = [0]
-    with tf.Session() as sess:
-        sess.run(model.pretrained())
-        # "D://pyworks//yolo//videoplayback.mp4"
 
-        # cap=cv2.imread('test.jpg',cv2.IMREAD_COLOR)
-        cap = cv2.imread('image.JPG', cv2.IMREAD_COLOR)
+    try:
+        with tf.Session() as sess:
+            cond.acquire()  ### mutex_lock
+            sess.run(model.pretrained())
+            # "D://pyworks//yolo//videoplayback.mp4"
+            while turn.myTurn != 1: cond.wait() #추가해준거임 내 번호가 아닐경우 대기
 
-        img = cv2.resize(cap, (416, 416))
-        image = np.array(img).reshape(-1, 416, 416, 3)
-        start_time = time.time()
-        preds = sess.run(model.preds, {inputs: model.preprocess(image)})
+            # cap=cv2.imread('test.jpg',cv2.IMREAD_COLOR)
+            cap = cv2.imread('image.JPG', cv2.IMREAD_COLOR)
 
-        print(time.time() - start_time)
-        boxes = model.get_boxes(preds, image.shape[1:3])
-        cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+            img = cv2.resize(cap, (647, 647), cv2.INTER_AREA)
 
-        cv2.resizeWindow('image', 700, 700)
+            image = np.array(img).reshape(-1, 647, 647, 3)
+            start_time = time.time()
+            preds = sess.run(model.preds, {inputs: model.preprocess(image)})
 
-        # print("--- %s seconds ---" % (time.time() - start_time))
-        boxes1 = np.array(boxes)
-        for j in list_of_classes:
-            count = 0
-            if str(j) in classes:
-                lab = classes[str(j)]
-            if len(boxes1) != 0:
+            print(time.time() - start_time)
+            boxes = model.get_boxes(preds, image.shape[1:3])
+            cv2.namedWindow('image', cv2.WINDOW_NORMAL)
 
-                for i in range(len(boxes1[j])):
-                    box = boxes1[j][i]
+            cv2.resizeWindow('image', 700, 700)
 
-                    if boxes1[j][i][4] >= .40:
-                        count += 1
+            # print("--- %s seconds ---" % (time.time() - start_time))
+            boxes1 = np.array(boxes)
+            for j in list_of_classes:
+                count = 0
+                if str(j) in classes:
+                    lab = classes[str(j)]
+                if len(boxes1) != 0:
 
-                        cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 1)
-                        cv2.putText(img, lab, (box[0], box[1]), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 255),
-                                    lineType=cv2.LINE_AA)
-            print(lab, ": ", count)
-            ref3.set({'people_number': count})
-            ref1.set({'people_number': random.randint(0, 10)})
-            ref2.set({'people_number': random.randint(0, 10)})
-            ref4.set({'people_number': random.randint(0, 10)})
+                    for i in range(len(boxes1[j])):
+                        box = boxes1[j][i]
+
+                        if boxes1[j][i][4] >= .40:
+                            count += 1
+
+                            cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 1)
+                            cv2.putText(img, lab, (box[0], box[1]), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 255),
+                                        lineType=cv2.LINE_AA)
+                print(lab, ": ", count)
+                ref3.set({'people_number': count})
+                ref1.set({'people_number': random.randint(0, 10)})
+                ref2.set({'people_number': random.randint(0, 10)})
+                ref4.set({'people_number': random.randint(0, 10)})
 
 
-        cv2.imshow("IMAGE", img)
+            cv2.imwrite("result_image.png", img)
 
-    cv2.destroyAllWindows()
+            fileUpload("result_image.png")
+            print("save end")
 
-    print(name2)
+        cv2.destroyAllWindows()
+        turn.myTurn = 0
+        cond.notifyAll()  # notify to all consumers
+        cond.release()  ### mutex_unlock
 
+        print('deeplearning end')
+
+    except Exception as e:
+        print(str(e))
+
+
+class CondVar :
+    myTurn = 0
+    def __init__ (self):
+        pass
 
 if __name__ == '__main__':
-    cred = credentials.Certificate('myKey.json')
-    firebase_admin.initialize_app(cred, {
-        'databaseURL': 'https://graduate-work-462b3.firebaseio.com'
-    })
-
-    ref1 = db.reference('Distribution_DB/Edong')
+    cond = threading.Condition()
+    turn = CondVar()
+    ref1 = db.reference('Distribution_DB/JongHap')
     ref2 = db.reference('Distribution_DB/Sanyung')
-    ref3 = db.reference('Distribution_DB/Kpu')
+    ref3 = db.reference('Distribution_DB/Tip')
     ref4 = db.reference('Distribution_DB/Olive')
 
-    for i in range(3):
-        t1 = threading.Thread(target=record, args=('record run', 'record end'))
-        t2 = threading.Thread(target=deeplearning, args=('deeplearning run', 'deeplearning end'))
+    for i in range(1):
+        t1 = threading.Thread(target=record, args=(cond,turn))
+        t2 = threading.Thread(target=deeplearning, args=(cond,turn))
         t1.start()
         t2.start()
         t1.join()
@@ -118,4 +163,5 @@ if __name__ == '__main__':
 
 
     print("===exit===")
+
 
