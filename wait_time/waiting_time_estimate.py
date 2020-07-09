80ccd9425825e0d3b911dbd62689d0c740bf6646
norzest <51143484+norzest@users.noreply.github.com>
2020-07-09 17:29:12 +0900

    Add files via upload

--- wait_time/waiting_time_estimate.py
+++ wait_time/waiting_time_estimate.py
@@ -145,10 +145,10 @@ class Calculate(Suite):
         cal[0]: dict(p1=2, p2=3, p3=1, p4=4, p5=2, p6=1, p7=0, p8=1, p9=8, p10=2, p11=1, p12=7),
         cal[1]: dict(p1=3, p2=6, p3=7, p4=1, p5=7, p6=1, p7=9, p8=9, p9=1, p10=2, p11=2, p12=1),
         cal[2]: dict(p1=2, p2=4, p3=3, p4=1, p5=2, p6=1, p7=3, p8=1, p9=11, p10=12, p11=1, p12=10),
-        cal[3]: dict(p1=1, p2=0, p3=2, p4=5, p5=3, p6=8, p7=6, p8=4, p9=1, p10=9, p11=11, p12=7),
-        cal[4]: dict(p1=1, p2=2, p3=0, p4=3, p5=1, p6=5, p7=1, p8=1, p9=2, p10=10, p11=1, p12=9),
+        cal[3]: dict(p1=1, p2=1, p3=2, p4=5, p5=3, p6=8, p7=6, p8=4, p9=1, p10=9, p11=11, p12=7),
+        cal[4]: dict(p1=1, p2=2, p3=1, p4=3, p5=1, p6=5, p7=1, p8=1, p9=2, p10=10, p11=1, p12=9),
         cal[5]: dict(p1=1, p2=1, p3=1, p4=2, p5=7, p6=3, p7=11, p8=8, p9=5, p10=3, p11=6, p12=1),
-        cal[6]: dict(p1=10, p2=10, p13=4, p4=4, p5=1, p6=6, p7=7, p8=15, p9=11, p10=13, p11=4, p12=5),
+        cal[6]: dict(p1=10, p2=10, p3=4, p4=4, p5=1, p6=6, p7=7, p8=15, p9=11, p10=13, p11=4, p12=5),
         cal[7]: dict(p1=12, p2=13, p3=4, p4=0, p5=2, p6=3, p7=7, p8=6, p9=5, p10=11, p11=8, p12=7),
         cal[8]: dict(p1=1, p2=4, p3=6, p4=7, p5=11, p6=16, p7=5, p8=5, p9=12, p10=10, p11=11, p12=1),
         cal[9]: dict(p1=0, p2=0, p3=16, p4=5, p5=4, p6=13, p7=10, p8=12, p9=10, p10=14, p11=15, p12=12),
@@ -168,7 +168,7 @@ class Calculate(Suite):
         cal[3]: dict(p1=1, p2=0, p3=2, p4=5, p5=3, p6=8, p7=6, p8=4, p9=1, p10=9, p11=11, p12=7),
         cal[4]: dict(p1=1, p2=2, p3=0, p4=3, p5=1, p6=5, p7=1, p8=1, p9=2, p10=10, p11=1, p12=9),
         cal[5]: dict(p1=1, p2=1, p3=1, p4=2, p5=7, p6=3, p7=11, p8=8, p9=5, p10=3, p11=6, p12=1),
-        cal[6]: dict(p1=10, p2=10, p13=4, p4=4, p5=1, p6=6, p7=7, p8=15, p9=11, p10=13, p11=4, p12=5),
+        cal[6]: dict(p1=10, p2=10, p3=4, p4=4, p5=1, p6=6, p7=7, p8=15, p9=11, p10=13, p11=4, p12=5),
         cal[7]: dict(p1=12, p2=13, p3=4, p4=0, p5=2, p6=3, p7=7, p8=6, p9=5, p10=11, p11=8, p12=7),
         cal[8]: dict(p1=1, p2=4, p3=6, p4=7, p5=11, p6=16, p7=5, p8=5, p9=12, p10=10, p11=11, p12=1),
         cal[9]: dict(p1=0, p2=0, p3=16, p4=5, p5=4, p6=13, p7=10, p8=12, p9=10, p10=14, p11=15, p12=12),
@@ -188,7 +188,7 @@ class Calculate(Suite):
         cal[3]: dict(p1=1, p2=0, p3=2, p4=5, p5=3, p6=8, p7=6, p8=4, p9=1, p10=9, p11=11, p12=7),
         cal[4]: dict(p1=1, p2=2, p3=0, p4=3, p5=1, p6=5, p7=1, p8=1, p9=2, p10=10, p11=1, p12=9),
         cal[5]: dict(p1=1, p2=1, p3=1, p4=2, p5=7, p6=3, p7=11, p8=8, p9=5, p10=3, p11=6, p12=1),
-        cal[6]: dict(p1=10, p2=10, p13=4, p4=4, p5=1, p6=6, p7=7, p8=15, p9=11, p10=13, p11=4, p12=5),
+        cal[6]: dict(p1=10, p2=10, p3=4, p4=4, p5=1, p6=6, p7=7, p8=15, p9=11, p10=13, p11=4, p12=5),
         cal[7]: dict(p1=12, p2=13, p3=4, p4=0, p5=2, p6=3, p7=7, p8=6, p9=5, p10=11, p11=8, p12=7),
         cal[8]: dict(p1=1, p2=4, p3=6, p4=7, p5=11, p6=16, p7=5, p8=5, p9=12, p10=10, p11=11, p12=1),
         cal[9]: dict(p1=0, p2=0, p3=16, p4=5, p5=4, p6=13, p7=10, p8=12, p9=10, p10=14, p11=15, p12=12),
@@ -208,7 +208,7 @@ class Calculate(Suite):
         cal[3]: dict(p1=1, p2=0, p3=2, p4=5, p5=3, p6=8, p7=6, p8=4, p9=1, p10=9, p11=11, p12=7),
         cal[4]: dict(p1=1, p2=2, p3=0, p4=3, p5=1, p6=5, p7=1, p8=1, p9=2, p10=10, p11=1, p12=9),
         cal[5]: dict(p1=1, p2=1, p3=1, p4=2, p5=7, p6=3, p7=11, p8=8, p9=5, p10=3, p11=6, p12=1),
-        cal[6]: dict(p1=10, p2=10, p13=4, p4=4, p5=1, p6=6, p7=7, p8=15, p9=11, p10=13, p11=4, p12=5),
+        cal[6]: dict(p1=10, p2=10, p3=4, p4=4, p5=1, p6=6, p7=7, p8=15, p9=11, p10=13, p11=4, p12=5),
         cal[7]: dict(p1=12, p2=13, p3=4, p4=0, p5=2, p6=3, p7=7, p8=6, p9=5, p10=11, p11=8, p12=7),
         cal[8]: dict(p1=1, p2=4, p3=6, p4=7, p5=11, p6=16, p7=5, p8=5, p9=12, p10=10, p11=11, p12=1),
         cal[9]: dict(p1=0, p2=0, p3=16, p4=5, p5=4, p6=13, p7=10, p8=12, p9=10, p10=14, p11=15, p12=12),
@@ -242,7 +242,7 @@ class CondVar:
         pass
 
 
-def bayes_thrd(i_cond, i_turn):
+def bayes_inner_fnc(res_name):
     global di
     global PAST_finish_time  # 이전 조리 완료 시간 ( ti-1 )
     global count
@@ -252,249 +252,114 @@ def bayes_thrd(i_cond, i_turn):
 
     finish_time = 0
 
+    if res_name == 'JongHap':
+        r_snapshot = JongHap_real_time_track.get()
+    elif res_name == 'Olive':
+        r_snapshot = Olive_real_time_track.get()
+    elif res_name == 'Sanyung':
+        r_snapshot = Sanyung_real_time_track.get()
+    elif res_name == 'TIP':
+        r_snapshot = TIP_real_time_track.get()
+
+    if r_snapshot is None:
+        print('대기중 메뉴 없음')
+        db.reference('Waiting_time_DB/%s/Waiting_time' % res_name).set(str(0))
+    else:
+        di = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        PAST_finish_time = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  # 이전 조리 완료 시간 ( ti-1 )
+        count = 0
+        cnt = 0
+        sample_num = 1
+
+        for R_key in r_snapshot:
+            if v is None:
+                break
+            else:
+                # 결제 음식
+                r_order_food = db.reference('Restaurant_DB/Real_time/%s/%s/order_food' % (res_name, R_key)).get()
+                v.append(r_order_food)
+
+        for R_key in r_snapshot:
+            if v is None:
+                break
+
+            # 결제 음식
+            r_order_food = db.reference('Restaurant_DB/Real_time/%s/%s/order_food' % (res_name, R_key)).get()
+
+            print(v)
+            if res_name == 'JongHap':
+                count = jonghap_list_insert_count(r_order_food)
+            elif res_name == 'Olive':
+                count = olive_list_insert_count(r_order_food)
+            elif res_name == 'Sanyung':
+                count = sanyung_list_insert_count(r_order_food)
+            elif res_name == 'TIP':
+                count = tip_list_insert_count(r_order_food)
+
+            # 결제 시간 ( pi )
+            r_pay_time = db.reference('Restaurant_DB/Real_time/%s/%s/pay_time' % (res_name, R_key)).get()
+            pre_finish_time = int(r_pay_time) + finish_time  # 현재 조리 완료 시간 ( ti )
+            if PAST_finish_time[count] > int(r_pay_time):
+                di[count] = pre_finish_time - PAST_finish_time[count]
+                if di[count] < 0:
+                    di[count] = pre_finish_time - int(r_pay_time)
+                else:
+                    di[count] = pre_finish_time - int(r_pay_time)
+
+            PAST_finish_time[count] = pre_finish_time
+
+            del v[0]
+
+            pmf = Calculate(hypos)
+            pmf.Normalize()
+            pmf.Update('p%d' % (count + 1))
+
+            max_num = 0
+
+            for hypo, prob in pmf.Items():
+                if prob >= max_num:
+                    max_num = prob
+                    end_hypo = hypo
+
+            cnt += int(end_hypo)
+            print(end_hypo, max_num)
+
+            print('================================================')
+
+        db.reference('Waiting_time_DB/%s/Waiting_time' % res_name).set(str(cnt))
+
+
+def delete_inner_fnc(res_name):
+    if res_name == 'JongHap':
+        r_snapshot = JongHap_real_time_track.get()
+    elif res_name == 'Olive':
+        r_snapshot = Olive_real_time_track.get()
+    elif res_name == 'Sanyung':
+        r_snapshot = Sanyung_real_time_track.get()
+    elif res_name == 'TIP':
+        r_snapshot = TIP_real_time_track.get()
+
+    if r_snapshot is None:
+        print('대기중 메뉴 없음')
+    else:
+        once = 0
+
+        for R_key in r_snapshot:
+            if once >= 1:
+                break
+            else:
+                db.reference('Restaurant_DB/Real_time/%s/%s' % (res_name, R_key)).delete()  # 결제 음식
+                once += 1
+
+
+def bayes_thrd(i_cond, i_turn):
     while True:
         i_cond.acquire()  # mutex_lock
 
-        jonghap_r_snapshot = JongHap_real_time_track.get()
-
-        if jonghap_r_snapshot is None:
-            print('대기중 메뉴 없음')
-            db.reference('Waiting_time_DB/JongHap/Waiting_time').set(str(0))
-        else:
-            di = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-            PAST_finish_time = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  # 이전 조리 완료 시간 ( ti-1 )
-            count = 0
-            cnt = 0
-            sample_num = 1
-
-            for R_key in jonghap_r_snapshot:
-                if v is None:
-                    break
-                else:
-                    r_order_food = db.reference('Restaurant_DB/Real_time/JongHap/%s/order_food' % R_key).get()  # 결제 음식
-                    v.append(r_order_food)
-
-            for R_key in jonghap_r_snapshot:
-                if v is None:
-                    break
-
-                r_order_food = db.reference('Restaurant_DB/Real_time/JongHap/%s/order_food' % R_key).get()  # 결제 음식
-
-                print(v)
-                count = jonghap_list_insert_count(r_order_food)
-                r_pay_time = db.reference('Restaurant_DB/Real_time/JongHap/%s/pay_time' % R_key).get()  # 결제 시간 ( pi )
-                pre_finish_time = int(r_pay_time) + finish_time  # 현재 조리 완료 시간 ( ti )
-                if PAST_finish_time[count] > int(r_pay_time):
-                    di[count] = pre_finish_time - PAST_finish_time[count]
-                    if di[count] < 0:
-                        di[count] = pre_finish_time - int(r_pay_time)
-                    else:
-                        di[count] = pre_finish_time - int(r_pay_time)
-
-                PAST_finish_time[count] = pre_finish_time
-
-                del v[0]
-
-                pmf = Calculate(hypos)
-                pmf.Normalize()
-                pmf.Update('p%d' % (count + 1))
-
-                max_num = 0
-
-                for hypo, prob in pmf.Items():
-                    if prob >= max_num:
-                        max_num = prob
-                        end_hypo = hypo
-
-                cnt += int(end_hypo)
-                print(end_hypo, max_num)
-
-                print('================================================')
-
-            db.reference('Waiting_time_DB/JongHap/Waiting_time').set(str(cnt))
-
-        # ###############################################################################################################################
-
-        olive_r_snapshot = Olive_real_time_track.get()
-
-        if olive_r_snapshot is None:
-            print('대기중 메뉴 없음')
-            db.reference('Waiting_time_DB/Olive/Waiting_time').set(str(0))
-        else:
-            di = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-            PAST_finish_time = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  # 이전 조리 완료 시간 ( ti-1 )
-            count = 0
-            cnt = 0
-            sample_num = 2
-
-            for R_key in olive_r_snapshot:
-                if v is None:
-                    break
-                else:
-                    r_order_food = db.reference('Restaurant_DB/Real_time/Olive/%s/order_food' % R_key).get()  # 결제 음식
-                    v.append(r_order_food)
-
-            for R_key in olive_r_snapshot:
-                if v is None:
-                    break
-
-                r_order_food = db.reference('Restaurant_DB/Real_time/Olive/%s/order_food' % R_key).get()  # 결제 음식
-
-                print(v)
-                count = olive_list_insert_count(r_order_food)
-                r_pay_time = db.reference('Restaurant_DB/Real_time/Olive/%s/pay_time' % R_key).get()  # 결제 시간 ( pi )
-                pre_finish_time = int(r_pay_time) + finish_time  # 현재 조리 완료 시간 ( ti )
-
-                if PAST_finish_time[count] > int(r_pay_time):
-                    di[count] = pre_finish_time - PAST_finish_time[count]
-                    if di[count] < 0:
-                        di[count] = pre_finish_time - int(r_pay_time)
-                    else:
-                        di[count] = pre_finish_time - int(r_pay_time)
-
-                PAST_finish_time[count] = pre_finish_time
-
-                del v[0]
-
-                pmf = Calculate(hypos)
-                pmf.Normalize()
-                pmf.Update('p%d' % (count + 1))
-
-                max_num = 0
-
-                for hypo, prob in pmf.Items():
-                    if prob >= max_num:
-                        max_num = prob
-                        end_hypo = hypo
-
-                cnt += int(end_hypo)
-                print(end_hypo, max_num)
-
-                print('================================================')
-
-            db.reference('Waiting_time_DB/Olive/Waiting_time').set(str(cnt))
-
-        # ###############################################################################################################################
-
-        sanyung_r_snapshot = Sanyung_real_time_track.get()
-
-        if sanyung_r_snapshot is None:
-            print('대기중 메뉴 없음')
-            db.reference('Waiting_time_DB/Sanyung/Waiting_time').set(str(0))
-        else:
-            di = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-            PAST_finish_time = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  # 이전 조리 완료 시간 ( ti-1 )
-            count = 0
-            cnt = 0
-            sample_num = 3
-
-            for R_key in sanyung_r_snapshot:
-                if v is None:
-                    break
-                else:
-                    r_order_food = db.reference('Restaurant_DB/Real_time/Sanyung/%s/order_food' % R_key).get()  # 결제 음식
-                    v.append(r_order_food)
-
-            for R_key in sanyung_r_snapshot:
-                if v is None:
-                    break
-
-                r_order_food = db.reference('Restaurant_DB/Real_time/Sanyung/%s/order_food' % R_key).get()  # 결제 음식
-
-                print(v)
-                count = sanyung_list_insert_count(r_order_food)
-                r_pay_time = db.reference('Restaurant_DB/Real_time/Sanyung/%s/pay_time' % R_key).get()  # 결제 시간 ( pi )
-                pre_finish_time = int(r_pay_time) + finish_time  # 현재 조리 완료 시간 ( ti )
-
-                if PAST_finish_time[count] > int(r_pay_time):
-                    di[count] = pre_finish_time - PAST_finish_time[count]
-                    if di[count] < 0:
-                        di[count] = pre_finish_time - int(r_pay_time)
-                    else:
-                        di[count] = pre_finish_time - int(r_pay_time)
-
-                PAST_finish_time[count] = pre_finish_time
-
-                del v[0]
-
-                pmf = Calculate(hypos)
-                pmf.Normalize()
-                pmf.Update('p%d' % (count + 1))
-
-                max_num = 0
-
-                for hypo, prob in pmf.Items():
-                    if prob >= max_num:
-                        max_num = prob
-                        end_hypo = hypo
-
-                cnt += int(end_hypo)
-                print(end_hypo, max_num)
-
-                print('================================================')
-
-            db.reference('Waiting_time_DB/Sanyung/Waiting_time').set(str(cnt))
-
-        # ###############################################################################################################################
-
-        tip_r_snapshot = TIP_real_time_track.get()
-
-        if tip_r_snapshot is None:
-            print('대기중 메뉴 없음')
-            db.reference('Waiting_time_DB/Tip/Waiting_time').set(str(0))
-        else:
-            di = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-            PAST_finish_time = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  # 이전 조리 완료 시간 ( ti-1 )
-            count = 0
-            cnt = 0
-            sample_num = 4
-
-            for R_key in tip_r_snapshot:
-                if v is None:
-                    break
-                else:
-                    r_order_food = db.reference('Restaurant_DB/Real_time/TIP/%s/order_food' % R_key).get()  # 결제 음식
-                    v.append(r_order_food)
-
-            for R_key in tip_r_snapshot:
-                if v is None:
-                    break
-
-                r_order_food = db.reference('Restaurant_DB/Real_time/TIP/%s/order_food' % R_key).get()  # 결제 음식
-
-                print(v)
-                count = tip_list_insert_count(r_order_food)
-                r_pay_time = db.reference('Restaurant_DB/Real_time/TIP/%s/pay_time' % R_key).get()  # 결제 시간 ( pi )
-                pre_finish_time = int(r_pay_time) + finish_time  # 현재 조리 완료 시간 ( ti )
-
-                if PAST_finish_time[count] > int(r_pay_time):
-                    di[count] = pre_finish_time - PAST_finish_time[count]
-                    if di[count] < 0:
-                        di[count] = pre_finish_time - int(r_pay_time)
-                    else:
-                        di[count] = pre_finish_time - int(r_pay_time)
-
-                PAST_finish_time[count] = pre_finish_time
-
-                del v[0]
-
-                pmf = Calculate(hypos)
-                pmf.Normalize()
-                pmf.Update('p%d' % (count + 1))
-
-                max_num = 0
-
-                for hypo, prob in pmf.Items():
-                    if prob >= max_num:
-                        max_num = prob
-                        end_hypo = hypo
-
-                cnt += int(end_hypo)
-                print(end_hypo, max_num)
-
-                print('================================================')
-
-            db.reference('Waiting_time_DB/Tip/Waiting_time').set(str(cnt))
+        bayes_inner_fnc('JongHap')
+        bayes_inner_fnc('Olive')
+        bayes_inner_fnc('Sanyung')
+        bayes_inner_fnc('TIP')
 
         i_turn.myTurn = 1
 
@@ -511,61 +376,10 @@ def delete_order(i_cond, i_turn):
         finish_time = random.randint(3, 10)
         i_cond.acquire()  # mutex_lock
 
-        jonghap_r_snapshot = JongHap_real_time_track.get()
-
-        if jonghap_r_snapshot is None:
-            print('대기중 메뉴 없음')
-        else:
-            once = 0
-
-            for R_key in jonghap_r_snapshot:
-                if once >= 1:
-                    break
-                else:
-                    db.reference('Restaurant_DB/Real_time/JongHap/%s' % R_key).delete()  # 결제 음식
-                    once += 1
-
-        olive_r_snapshot = Olive_real_time_track.get()
-
-        if olive_r_snapshot is None:
-            print('대기중 메뉴 없음')
-        else:
-            once = 0
-
-            for R_key in olive_r_snapshot:
-                if once >= 1:
-                    break
-                else:
-                    db.reference('Restaurant_DB/Real_time/Olive/%s' % R_key).delete()  # 결제 음식
-                    once += 1
-
-        sanyung_r_snapshot = Sanyung_real_time_track.get()
-
-        if sanyung_r_snapshot is None:
-            print('대기중 메뉴 없음')
-        else:
-            once = 0
-
-            for R_key in sanyung_r_snapshot:
-                if once >= 1:
-                    break
-                else:
-                    db.reference('Restaurant_DB/Real_time/Sanyung/%s' % R_key).delete()  # 결제 음식
-                    once += 1
-
-        tip_r_snapshot = TIP_real_time_track.get()
-
-        if tip_r_snapshot is None:
-            print('대기중 메뉴 없음')
-        else:
-            once = 0
-
-            for R_key in tip_r_snapshot:
-                if once >= 1:
-                    break
-                else:
-                    db.reference('Restaurant_DB/Real_time/TIP/%s' % R_key).delete()  # 결제 음식
-                    once += 1
+        delete_inner_fnc('JongHap')
+        delete_inner_fnc('Olive')
+        delete_inner_fnc('Sanyung')
+        delete_inner_fnc('TIP')
 
         i_turn.myTurn = 1
 
