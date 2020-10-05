9df4e74a41a17c0d22ee51fe2f61dae6246bd2b4
cbsook77 <61221756+cbsook77@users.noreply.github.com>
2020-10-05 20:58:05 +0900

    Update JongHap.py

--- TestStub/JongHap.py
+++ TestStub/JongHap.py
@@ -15,6 +15,7 @@ firebase_admin.initialize_app(cred, {
     'storageBucket': 'graduate-work-462b3.appspot.com'
 })
 
+
 bucket= storage.bucket()
 #blob=bucket.blob("food_img/JongHap/1.jpg")
 #image=blob.download_as_string("1")
